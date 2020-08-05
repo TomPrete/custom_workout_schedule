7e6b60f02d76c047a0ada055f3023541ebbda1e7
Tom Prete <prete316@gmail.com>
2020-08-05 16:59:14 -0500

    added pages and links

--- workouts/urls.py
+++ workouts/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     path('', views.main_app, name='main_app'),
     path('programs', views.all_programs, name='all_programs'),
     path('workouts', views.all_workouts, name='all_workouts'),
-    path('workout/<str:workout_id>', views.workout_detail, name='workout_detail'),
+    path('workout/<int:workout_id>', views.workout_detail, name='workout_detail'),
+    path('category/<int:category_id>', views.category_detail, name='category_detail'),
     path('products', views.affiliate_page, name='affiliate_page'),
+    path('download-csv/<str:schedule>', views.save_workout_to_csv, name='save_workout_to_csv'),
 ]
--- workouts/views.py
+++ workouts/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from .models import Program, Workout, Category, Type, MuscleGroup
 from .generate_schedule import Schedule
+from collections import Counter
+import csv
+from ast import literal_eval
 
 def main_app(request):
   programs = Program.objects.all()
   week = [3, 4, 5, 6, 7]
+  final_schedule = None
   if request.method == "POST":
     programs_selected = request.POST.getlist('programs')
     if len(programs_selected) == 0:
@@ -16,6 +21,32 @@ def main_app(request):
     return render(request, 'workouts/home.html', {'programs': programs, 'week': week, 'schedule': final_schedule})
   return render(request, 'workouts/home.html', {'programs': programs, 'week': week})
 
+def save_workout_to_csv(request, schedule):
+  response = HttpResponse(content_type='text/csv')
+  response['Content-Disposition'] = 'attachment; filename="custom_workout_schedule.csv"'
+  print(request)
+  print(response)
+  schedule.replace('%20', ' ')
+  schedule = literal_eval(schedule)
+  print(schedule)
+  writer = csv.writer(response)
+  writer.writerow(['First row', 'Foo', 'Bar', 'Baz'])
+  writer.writerow(['Second row', 'A', 'B', 'C', '"Testing"', "Here's a quote"])
+
+  return response
+  # with open('custom_workout_schedule.csv', mode='w') as schedule_file:
+  #   fieldnames = ['week', 'day', 'workout', 'program', 'minutes', 'categories']
+  #   writer = csv.DictWriter(schedule_file, fieldnames=fieldnames)
+  #   writer.writeheader()
+  #   for index, value in enumerate(final_schedule):
+  #     for key in value:
+  #       writer.writerow({
+  #         'week': index+1,
+  #         'day': key,
+  #         'workout': value[key],
+  #         'program': value[key].program,
+  #         'minutes': value[key].time
+  #       })
 
 def _get_program_workouts(programs):
   workouts = Program.objects.get(program=programs[0]).workouts.all()
@@ -38,13 +69,41 @@ def all_programs(request):
 
 def workout_detail(request, workout_id):
   workout = Workout.objects.get(id=workout_id)
-  program_workouts = []
-  return render(request, 'workouts/workout_detail.html', {'workout': workout})
+  related_workouts = _find_related_workouts(workout)
+  return render(request, 'workouts/workout_detail.html', {'workout': workout, 'related_workouts': related_workouts})
 
+# Get related workouts based on categories
+def _find_related_workouts(workout):
+  workouts_query = []
+  list_of_workouts_in_category = []
+  related_workouts = []
+  if workout.category.all().count() == 1:
+    list_of_workouts_in_category = workout.category.first().workouts.all()
+    for related_workout in list_of_workouts_in_category:
+      if related_workout != workout:
+        related_workouts.append(related_workout)
+  else:
+    for category in workout.category.all():
+      workouts_query.append(category.workouts.all())
+    for list in workouts_query:
+      for related_workout in list:
+        list_of_workouts_in_category.append(related_workout)
+    list_of_workouts_in_category = [k for k,v in Counter(list_of_workouts_in_category).items() if v>1]
+    for related_workout in list_of_workouts_in_category:
+        if related_workout != workout:
+          related_workouts.append(related_workout)
+  return related_workouts
 
 def all_workouts(request):
   workouts = Workout.objects.all().order_by()
   return render(request, 'workouts/all_workouts.html', {'all_workouts': workouts})
+
+
+
+def category_detail(request, category_id):
+  category = Category.objects.get(id=category_id)
+  all_workouts_in_category = Category.objects.get(id=category_id).workouts.all()
+  return render(request, 'workouts/category_detail.html', {'category': category, 'all_workouts_in_category': all_workouts_in_category})
 
 
 def affiliate_page(request):
