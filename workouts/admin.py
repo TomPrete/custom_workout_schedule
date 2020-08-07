56af643bbe6f6f57c7b5729e1d4353de1ddc2266
Tom Prete <prete316@gmail.com>
2020-08-07 18:35:01 -0500

    updated admin models

--- workouts/admin.py
+++ workouts/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Program, Workout, Category, Type, MuscleGroup
+from .models import Program, Workout, Category, Type, MuscleGroup, Message, ScheduleDetail, Schedule
 
-all_models = [Program, Workout, Category, Type, MuscleGroup]
+all_models = [Program, Workout, Category, Type, MuscleGroup, Message, ScheduleDetail, Schedule]
 
 admin.site.register(all_models)
